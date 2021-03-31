c70db114d26bbea5def5477862b2a28097f13d90
Suresh Thomas <suresh.thomas@ncr.com>
2021-03-31 10:57:36 +0100

    added a bug for lint to catch in hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 import click
+var=
 
 @click.command(help="This is just a hello app. It does nothing.")
-@click.option("--name", prompt="I need your name", help="Need name")
-@click.option("--color", prompt="I need your color", help="This is your color")
+@click.option("--name", prompt="I need your name", help="Need name" ,default="Suresh")
+@click.option("--color", prompt="I need your color", help="This is your color", default = "Blue")
 def hello(name, color):
     if name == "Thor":
         click.echo("Thor, you are always red.")
